29504ba6d72fc8746fe7fba13ec48dae6e918373
zealptl <zealpatel06@gmail.com>
2020-02-22 23:29:34 -0500

    added getters and setters to MyShape

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -19,24 +19,59 @@ public class Main extends Application {
         Canvas canvas = new Canvas(w, h);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        gc.setLineWidth(2);
-        gc.strokeRect(25, 25, w - 50, h - 50);
-        gc.strokeLine(25,25, w - 25, h - 25);
-        gc.strokeLine(w - 25,25, 25, h - 25);
-        gc.fillPolygon(new double[]{
-             200.0 , 300.0, 350.0, 300.0, 200.0, 150.0
-        }, new double[] {
-                50.0, 50.0, 150.0, 250.0, 250.0, 150.0
-        },6);
+        gc.setFill(Color.LIGHTGRAY);
+        double hex1Len = 140;
+        strokeHexagon(w/2,h/2,hex1Len,gc);
+        fillHexagon(w/2, h/2, hex1Len, gc);
 
+        gc.setFill(Color.YELLOW);
+        double circle1Len = 2 * Math.sqrt(Math.pow(hex1Len, 2) - Math.pow(hex1Len/2, 2));
+        fillCircle(w/2, h/2, circle1Len, gc);
 
+        gc.setFill(Color.DARKSEAGREEN);
+        double hex2Len = circle1Len / 2;
+        fillHexagon(w/2,h/2,  hex2Len, gc);
 
+        gc.setFill(Color.LIGHTPINK);
+        double circle2Len = 2 * Math.sqrt(Math.pow(hex2Len, 2) - Math.pow(hex2Len/2, 2));
+        fillCircle(w/2,h/2, circle2Len, gc);
+
+        gc.setFill(Color.LIGHTBLUE);
+        double hex3Len = circle2Len/2;
+        fillHexagon(w/2,h/2, hex3Len, gc);
+        gc.strokeRect(50, 50, w - 100, h - 100);
+        gc.strokeLine(50,50, w - 50, h - 50);
+        gc.strokeLine(w - 50,50, 50, h - 50);
         root.getChildren().add(canvas);
         Scene sc = new Scene(root, w, h);
         primaryStage.setScene(sc);
         primaryStage.show();
     }
 
+    public double[] getXPoints(int x, double len) {
+        double leg = Math.sqrt(Math.pow(len, 2) - Math.pow(len/2, 2));
+        return new double[]{x, x + leg, x + leg, x, x - leg, x - leg};
+    }
+
+    public double[] getYPoints(int y, double len) {
+        return new double[]{y - len, y - (len/2), y + (len/2), y + len, y + (len/2), y - (len/2)};
+    }
+
+    public void strokeHexagon(int x, int y, double len, GraphicsContext gc) {
+        double[] xPoints = getXPoints(x, len);
+        double[] yPoints = getYPoints(y, len);
+        gc.strokePolygon(xPoints, yPoints, 6);
+    }
+
+    public void fillHexagon(int x, int y, double len, GraphicsContext gc) {
+        double[] xPoints = getXPoints(x, len);
+        double[] yPoints = getYPoints(y, len);
+        gc.fillPolygon(xPoints, yPoints, 6);
+    }
+
+    public void fillCircle(int x, int y, double radius, GraphicsContext gc) {
+        gc.fillOval(x - (radius/2), y - (radius/2), radius, radius);
+    }
 
     public static void main(String[] args) {
         launch(args);
--- src/sample/MyShape.java
+++ src/sample/MyShape.java
@@ -1,4 +1,29 @@
 package sample;
 
 public class MyShape {
+    private int x;
+    private int y;
+
+    public MyShape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+
 }
