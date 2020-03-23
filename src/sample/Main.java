fe586082814e78869b18059d9754ea1c3939ede5
zealptl <zealpatel06@gmail.com>
2020-03-22 21:40:40 -0400

    added some methods

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -56,30 +56,36 @@ public class Main extends Application {
         MyRectangle rect1 = new MyRectangle(centerX, centerY,rect1Width, rect1Height);
         rect1.setColor(MyColor.BIEGE);
         rect1.draw(gc);
+        System.out.println("-------\n" + rect1.toString());
 
         MyOval oval1 = new MyOval(centerX, centerY, rect1Width, rect1Height);
         oval1.setColor(MyColor.LIGHTGREEN);
         oval1.draw(gc);
+        System.out.println("-------\n" + oval1.toString());
 
         double rect2Width = rect1Width / Math.sqrt(2);
         double rect2Height = rect1Height / Math.sqrt(2);
         MyRectangle rect2 = new MyRectangle(centerX, centerY, rect2Width, rect2Height);
         rect2.setColor(MyColor.LIGHTGRAY);
         rect2.draw(gc);
+        System.out.println("-------\n" + rect2.toString());
 
         MyOval oval2 = new MyOval(centerX, centerY, rect2Width, rect2Height);
         oval2.setColor(MyColor.LIGHTBLUE);
         oval2.draw(gc);
+        System.out.println("-------\n" + oval2.toString());
 
         double rect3Width = rect2Width / Math.sqrt(2);
         double rect3Height = rect2Height / Math.sqrt(2);
         MyRectangle rect3 = new MyRectangle(centerX, centerY, rect3Width, rect3Height);
         rect3.setColor(MyColor.LIGHTPINK);
         rect3.draw(gc);
+        System.out.println("-------\n" + rect3.toString());
 
         MyOval oval3 = new MyOval(centerX, centerY, rect3Width, rect3Height);
         oval3.setColor(MyColor.YELLOW);
         oval3.draw(gc);
+        System.out.println("-------\n" + oval3.toString());
 
         MyLine topBorder = new MyLine(w*0.05, h*0.05, w*0.95, h*0.05);
         MyLine rightBorder = new MyLine(w*0.95, h*0.05, w*0.95, h*0.95);
--- src/sample/MyOval.java
+++ src/sample/MyOval.java
@@ -19,9 +19,32 @@ public class MyOval extends MyShape {
         this.yLength = yLength;
     }
 
+    public double getxLength() { return xLength; }
+
+    public void setxLength(double xLength) { this.xLength = xLength; }
+
+    public double getyLength() {
+        return yLength;
+    }
+
+    public void setyLength(double yLength) {
+        this.yLength = yLength;
+    }
+
+    public double getPerimeter() {
+        double a = this.xLength/2;
+        double b = this.yLength/2;
+        return Math.PI * ((3*(a+b)) - Math.sqrt((3*a + b)*(a + 3*b)));
+    }
+
+    public double getArea() {
+        return Math.PI * (this.xLength/2) * this.yLength/2;
+    }
+
     @Override
     public String toString() {
-        return "";
+        return this.getClass().getName() + "\nX Axis length: " + Double.toString(this.xLength) + "\nY Axis length: " + Double.toString(this.yLength)
+                + "\nPerimeter: " + Double.toString(this.getPerimeter()) + "\nArea: " + Double.toString(this.getArea());
     }
 
     @Override
